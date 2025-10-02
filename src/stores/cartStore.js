ad5f28157840db136636f763ed634c0319731224
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-10-02 21:21:37 +0330

    finish home page

---
+++ src/stores/cartStore.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export const useCartStore = defineStore('cartStore', () => {
+  const cartItems = reactive([])
+
+  const cartCount = computed(() => {
+    return cartItems.reduce((total, item) => total + item.quantity, 0)
+  })
+  const cartTotal = computed(() => {
+    return cartItems.reduce((total, item) => total + item.quantity * item.price, 0)
+  })
+
+  function addToCart(menuItem, quantity = 1) {
+    const existingItem = cartItem.find((item) => item.id === menuItem.id)
+    if (existingItem) {
+      existingItem.quantity += quantity
+    } else {
+      cartItems.push({
+        id: menuItem.id,
+        name: menuItem.name,
+        image: menuItem.image,
+        quantity: quantity,
+        price: menuItem.price,
+      })
+    }
+  }
+  function clearCart() {
+    cartItems = []
+  }
+  return { cartItems, cartCount, cartTotal, addToCart, clearCart }
+})
